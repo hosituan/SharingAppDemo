ae7b7eed62d88d7ee0f65f7aa85fb170442f1cf1
hosituan <hosituan.work@gmail.com>
2022-12-11 23:01:44 +0700

    Initial Commit

---
+++ SharingAppDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharingAppDemo1
+//
+//  Created by Tuan Si Ho (3406) on 11/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
